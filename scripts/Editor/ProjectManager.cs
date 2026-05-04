ae81bf724bde9648219091a6f9c790fb84d86f79
zhixuan45 <78686872+zhixuan45@users.noreply.github.com>
2026-05-04 18:49:37 +0800

    Revert "🔒 Fix potential directory traversal in ProjectManager"

--- scripts/Editor/ProjectManager.cs
+++ scripts/Editor/ProjectManager.cs
@@ -107,13 +107,6 @@ public static class ProjectManager
         if (sourcePath.StartsWith("content://"))
         {
             fileName = System.Uri.UnescapeDataString(fileName).Replace(":", "_");
-            fileName = fileName.GetFile();
-        }
-
-        if (string.IsNullOrEmpty(fileName) || fileName == "." || fileName == "..")
-        {
-            ((SceneTree)Engine.GetMainLoop()).Root.GetNode<ErrorNotifier>("ErrorNotifier").ShowToast("Import failed: Invalid file name.");
-            return "";
         }
         
         // 尝试使用 Godot.FileAccess 读取文件内容 (原生支持 content:// 读取)
